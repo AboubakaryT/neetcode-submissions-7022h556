3ee04fa23192fcc866d2e4752516470da69dd37e
Aboubakary Traore <atraore.dev@gmail.com>
2026-06-20 11:47:14 -0500

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        '''
+        Maximum k can be at most the largest value in the array.
+        [1,1] 2 -> 1
+        [1,2] 2 -> 2
+        Bruteforce: 
+        '''
+        res = float('inf')
+        l = 1
+        r = max(piles)
+
+        while l <= r:
+            m = (r + l) // 2
+            hour = 0
+            for i in piles:
+                hour+= math.ceil(i / m)
+            print("m: ", m)
+            print("hour: ", hour)      
+            if hour <= h:
+                res = min(m, res)
+                r = m + -1
+            else:
+                l= m +1
+
+        return int(res)
+
