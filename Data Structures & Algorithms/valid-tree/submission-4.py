8cc66f12f4306fc4c32378c66a9dbde96e7e3c33
Aboubakary Traore <atraore.dev@gmail.com>
2026-08-30 22:34:45 -0400

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,32 @@
+from collections import deque as dq, defaultdict
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+            
+        adjList = defaultdict(list)
+        for f,t in edges:
+            adjList[f].append(t)
+            adjList[t].append(f)
+
+        visited = set()
+        queue = dq()
+        queue.append([0, -1])
+        visited.add(0)
+    
+        while queue:
+            node,parent = queue.popleft()
+            for neigh in adjList[node]:
+                if neigh == parent:
+                    continue
+                if neigh in visited:
+                    return False
+                visited.add(neigh)
+                queue.append([neigh,node])
+
+
+        
+        return len(visited) == n
+
+            
+        
